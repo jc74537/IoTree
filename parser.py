bb6f64b7acdfe4ef2dd4fe9b74645acda9e3a35b
jc74537 <jcook.74537@farnborough.ac.uk>
2018-12-15 00:19:44 +0000

    Improved robustness of the sound controller, added new patterns

--- parser.py
+++ parser.py
@@ -1,38 +1,57 @@
-input_ = """0 pwm 12
-wait 50
-0 pwm 25
-wait 50
-0 pwm 50
-1 pwm 12
-wait 50
-0 pwm 100
-1 pwm 25
-wait 50
-0 pwm 200
-1 pwm 50
-2 pwm 12
-wait 50
-1 pwm 100
-2 pwm 25
-wait 50
-0 pwm 0
-1 pwm 200
-2 pwm 50
-3 pwm 12
-wait 50
-2 pwm 100
-3 pwm 25
-wait 50
+input_ = """0 pwm 0
 1 pwm 0
-2 pwm 200
-3 pwm 50
-wait 50
-3 pwm 100
-wait 50
-3 pwm 200
 2 pwm 0
-wait 100
-3 pwm 0""".split("\n")
+3 pwm 0
+
+0 pwm 255
+wait 255
+wait 255
+wait 255
+wait 255
+
+1 pwm 255
+0 pwm 0
+wait 255
+wait 255
+wait 255
+wait 255
+
+2 pwm 255
+1 pwm 0
+wait 255
+wait 255
+wait 255
+wait 255
+
+
+3 pwm 255
+2 pwm 0
+wait 255
+wait 255
+wait 255
+wait 255
+
+2 pwm 255
+3 pwm 0
+wait 255
+wait 255
+wait 255
+wait 255
+
+1 pwm 255
+2 pwm 0
+wait 255
+wait 255
+wait 255
+wait 255
+
+0 pwm 255
+1 pwm 0
+wait 255
+wait 255
+wait 255
+wait 255
+""".split("\n")
 def helper(a) :
     if a=="pwm":
         return "true"
--- sound/sound.go
+++ sound/sound.go
@@ -1,24 +1,33 @@
 package sound
 
 import (
-	"github.com/tarm/serial"
 	"log"
+
+	"github.com/tarm/serial"
 )
+
 var (
-	c = &serial.Config{Name: "COM10", Baud: 9600}
+	c      = &serial.Config{Name: "COM10", Baud: 9600}
 	s, err = serial.OpenPort(c)
 )
 
 //Play a christmas song from an RTTTL string
-func Play(sound string) { 
-	_, err := s.Write([]byte(sound))
-	if err != nil {
-		//log.Fatal(err)
-		log.Println(err)
-		s, err = serial.OpenPort(c)
+func Play(sound string) {
+	if s != nil { //if a previous reconnect attempt failed, it may set s to nil
+		_, err := s.Write([]byte(sound))
 		if err != nil {
-			log.Println("Failed to reconnect: "+err.Error())
+			//log.Fatal(err)
+			log.Println(err)
+			s, err = serial.OpenPort(c)
+			if err != nil {
+				log.Println("Failed to reconnect: " + err.Error())
+			} else { //If it reconnects
+				log.Println("Reconnected sound device")
+				Play(sound) //try again
+			}
 		}
+	} else { //really, without making it set up the serial device again, it's not recoverable
+		log.Println("Sound device disconnected. This is not recoverable.")
 	}
-	
+
 }
